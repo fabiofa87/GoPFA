6d283081c6aa4b5ed03118820b0fd4de4cca28b8
fabiofa87 <fabiofadev87@gmail.com>
2023-05-09 17:13:20 -0300

    adicionado rabbitmq e grafana

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
 	"github.com/fabiofa8/pfa-go/internal/order/infra/database"
 	"github.com/fabiofa8/pfa-go/internal/order/usecase"
+	"github.com/fabiofa8/pfa-go/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 
 func main() {
+	maxWorkers := 4
+	wg := sync.WaitGroup{}
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
@@ -16,14 +23,36 @@ func main() {
 	defer db.Close()
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
-	input := usecase.OrderInputDTO{
-		ID:    "1234",
-		Price: 100,
-		Tax:   10,
-	}
-	output, err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
-	println(output.FinalPrice)
+	defer ch.Close()
+	deliveryMessage := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, deliveryMessage)
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		go worker(deliveryMessage, uc, i)
+		defer wg.Done()
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <- chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		input.Tax = 10.2
+
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		msg.Ack(false)
+		
+	}
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,15 @@ module github.com/fabiofa8/pfa-go
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/rabbitmq/amqp091-go v1.8.1
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.3.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
